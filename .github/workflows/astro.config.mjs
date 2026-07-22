21ff92949b4cafa66910b5def13b1caaa00d5b26
Jesús <jesusgallettooficial@gmail.com>
2026-07-22 03:40:58 -0300

    Add files via upload

---
+++ .github/workflows/astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({});
+  // Tu usuario de GitHub
+  site: 'https://matrizindie.github.io',
+  
+  // Si el repo se llama distinto a "tu-usuario.github.io", pon el nombre aquí:
+  base: '/matrizindie', 
+});
